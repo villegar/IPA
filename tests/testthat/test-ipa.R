9dcc7cd498524cae1c2d5a8391ccb937eabcef0c
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-11 14:41:02 +0100

    Minor changes

--- tests/testthat/test-ipa.R
+++ tests/testthat/test-ipa.R
@@ -2,15 +2,16 @@ test_that("RGB decomposition works", {
   test_data <- data.frame(name = c("R", "G", "B"), values = c(2, 2, 2))
   RGB <- c("red", "green", "blue")
   png("test_plot.png")
-  barplot(height = test_data$values, names = test_data$name, col = RGB)
+    barplot(height = test_data$values, names = test_data$name, col = RGB)
   dev.off()
-  rgb_decomposition(here::here(), "png", recursive = FALSE)
+  rgb_decomposition(".", "png", recursive = FALSE)
   # Check for generated histograms
   filenames <- c("test_plot.png",
                  "test_plot-blue.Rdata",
                  "test_plot-green.Rdata",
                  "test_plot-red.Rdata")
   for (f in filenames) {
+    message(paste0("Testing: ", f))
     expect_true(file.exists(f))
     expect_false(dir.exists(f))
     expect_gt(file.size(f), 0)
