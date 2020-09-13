031124d3b7e05d676add7fa251d851c31b98e544
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-13 14:12:06 +0100

    Adding path for test files

--- tests/testthat/test-ipa.R
+++ tests/testthat/test-ipa.R
@@ -1,15 +1,17 @@
 test_that("RGB decomposition works", {
   test_data <- data.frame(name = c("R", "G", "B"), values = c(2, 2, 2))
   RGB <- c("red", "green", "blue")
-  png("test_plot.png")
+  png(here::here("test_plot.png"))
     barplot(height = test_data$values, names = test_data$name, col = RGB)
   dev.off()
-  rgb_decomposition(".", "png", recursive = FALSE)
+  rgb_decomposition(here::here(), "png", recursive = FALSE)
   # Check for generated layer files
-  filenames <- c("test_plot.png",
-                 "test_plot-blue.Rdata",
-                 "test_plot-green.Rdata",
-                 "test_plot-red.Rdata")
+  filenames <- paste(here::here(),
+                     c("test_plot.png",
+                       "test_plot-blue.Rdata",
+                       "test_plot-green.Rdata",
+                       "test_plot-red.Rdata"),
+                     sep = "/")
   for (f in filenames) {
     message(paste0("Testing: ", f))
     expect_true(file.exists(f))
