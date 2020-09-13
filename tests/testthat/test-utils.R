154ae8abb5a3c3c82109b0963bcace1a5554aa83
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-13 10:37:00 +0100

    Adding tests for hex_logo

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("hexagonal logo works", {
+  hex_logo(output = "hex_logo.png")
+  expect_true(file.exists("hex_logo.png"))
+  expect_false(dir.exists("hex_logo.png"))
+  expect_gt(file.size("hex_logo.png"), 0)
+  file.remove("hex_logo.png")
+  expect_false(file.exists("hex_logo.png"))
 })
