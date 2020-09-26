b5d8551eb2c30891cc68ad70b603304dc6d2431f
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-26 12:32:46 +0100

    Adding tests for drop_extension

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,10 @@ test_that("hexagonal logo works", {
   file.remove("hex_logo.png")
   expect_false(file.exists("hex_logo.png"))
 })
+
+test_that("drop file extension works", {
+  with_path <- drop_extension("/path/to/A.txt")
+  without_path <- drop_extension("/path/to/A.txt", TRUE)
+  expect_equal(with_path, "/path/to/A")
+  expect_equal(without_path, "A")
+})
