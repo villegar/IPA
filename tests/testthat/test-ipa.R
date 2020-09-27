0f0ab3dbc2812054b3bc4294acd68db7a1f634df
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-27 13:17:42 +0100

    Adding tests for add_alpha

--- tests/testthat/test-ipa.R
+++ tests/testthat/test-ipa.R
@@ -44,3 +44,27 @@ test_that("Remove image background works", {
     expect_false(file.exists(f))
   }
 })
+
+test_that("Add transparency (alpha) works", {
+  # Create test image
+  red <- matrix(0, 50, 50)
+  red[1:25, 1:25] <- 1
+  blue <- matrix(0, 50, 50)
+  blue[26:50, 1:25] <- 1
+  green <- matrix(0, 50, 50)
+  green[1:25, 26:50] <- 1
+  alpha <- matrix(1, 50, 50)
+  alpha[26:50, 26:50] <- 0
+  img <- imager::as.cimg(abind::abind(imager::as.cimg(red),
+                                      imager::as.cimg(blue),
+                                      imager::as.cimg(green),
+                                      imager::as.cimg(alpha),
+                                      along = 4))
+  # Remove red portion of the image
+  img2 <- add_alpha(img, c(1, 25, 1, 25))
+  expect_equal(sum(as.matrix(imager::R(img2)) > 0), 0) # Zero red pixels
+  expect_error(add_alpha(img, c(1, 25, 1))) # wrong length for the area param.
+  expect_error(add_alpha(as.array(img), c(1, 25, 1, 25))) # wrong class
+  expect_error(add_alpha(as.array(img), c(100, 25, 1, 25))) # out of bounds
+  expect_message(img3 <- add_alpha(img, c(1, 25, 1, 25), quiet = FALSE))
+})
